5053ee9244a135fc374bcae51004bf60ab198b6e
Dominic Farquharson <farquharsondominic@gmail.com>
2018-04-01 22:53:13 -0400

    add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    contentBase: './dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'React Webpack',
+    }),
+  ],
+  devtool: 'inline-source-map',
+};
